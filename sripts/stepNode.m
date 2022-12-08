6123c33589f876d5f1354834fdb070625b0b693e
Klimaj Zoltan <zoltan.klimaj@gmail.com>
2022-12-08 17:05:52 +0100

    delete files

--- sripts/stepNode.m
+++
@@ -1,8 +0,0 @@
-function shiftStructStack = stepNode(shiftStructNodeStc,shiftStructNodeTmp,actualDay,iDoctor)
-shiftStructNodeTmp.requiredShiftArray(iDoctor)= shiftStructNodeTmp.requiredShiftArray(iDoctor)-1;
-shiftStructNodeTmp.doctorMatrix(:,actualDay) = 0;
-shiftStructNodeTmp.doctorMatrix(iDoctor,actualDay) = 1;
-% shiftStructNodeTmp.day = actualDay + 1;
-shiftStructNodeTmp.dayIndex = shiftStructNodeTmp.dayIndex + 1;
-shiftStructStack = {shiftStructNodeStc{:} shiftStructNodeTmp};
-end
